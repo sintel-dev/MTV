095ca85fcc2d8ab380c8ed279b9af854e7eef45f
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2020-10-13 09:03:59 +0300

    Filtering shapes typescript ready

--- client/src/components/Timeseries/Sidebar/SidebarComponents/SimilarShapes/FilterShapes.tsx
+++ client/src/components/Timeseries/Sidebar/SidebarComponents/SimilarShapes/FilterShapes.tsx
@@ -5,19 +5,28 @@ import { Collapse } from 'react-collapse';
 import './FilterShapes.scss';
 import { updateCurrentPercentage } from 'src/model/actions/similarShapes';
 import { TriangleDown, TriangleUp } from 'src/components/Common/icons';
+import { RootState } from 'src/model/types';
 
-const percentageCount = () => {
+const percentageCount = (): Array<number> => {
   const stepValues = [];
   for (let iterator = 0; iterator <= 100; iterator += 5) {
     stepValues.push(iterator);
   }
-
   return stepValues;
 };
 
-class FilterShapes extends Component {
+type OwnProps = {
+  toggleShapesFiltering: () => void;
+  isShapeFilteringVisible: boolean;
+};
+
+type StateProps = ReturnType<typeof mapState>;
+type DispatchProps = ReturnType<typeof mapDispatch>;
+type Props = StateProps & DispatchProps & OwnProps;
+
+class FilterShapes extends Component<Props> {
   renderStepHeight(percentage) {
-    const { grouppedShapes } = this.countSimilarity();
+    const grouppedShapes = this.countSimilarity();
     const graphHeight = 40;
     const shapeIndex = grouppedShapes.findIndex((similarity) => similarity[0] === percentage);
     if (shapeIndex === -1) {
@@ -28,12 +37,12 @@ class FilterShapes extends Component {
     return shapeHeight;
   }
 
-  grouppedIntervals() {
-    const steps = percentageCount();
-    const intervals = steps.reduce((acc, elem) => {
+  grouppedIntervals(): Array<[number, number]> {
+    const steps: Array<number> = percentageCount();
+    const intervals: Array<[number, number]> = steps.reduce((acc: Array<[number, number]>, elem: number) => {
       if (elem !== 100) {
-        const nextStep = elem + 5;
-        const newEntry = [elem, nextStep];
+        const nextStep: number = elem + 5;
+        const newEntry = [elem, nextStep] as [number, number];
         acc.push(newEntry);
       }
       return acc;
@@ -42,29 +51,29 @@ class FilterShapes extends Component {
     return intervals;
   }
 
-  countSimilarity() {
+  countSimilarity(): Array<[number, number]> {
     const { currentShapes } = this.props;
-    const intervals = this.grouppedIntervals();
-    const grouppedShapes = intervals.map((currentInterval) => {
+    const intervals: Array<[number, number]> = this.grouppedIntervals();
+    const grouppedShapes: Array<[number, number]> = intervals.map((currentInterval) => {
       const [min, max] = currentInterval;
       return [
         max,
         currentShapes.filter((shape) => shape.similarity * 100 >= min && shape.similarity * 100 <= max).length,
       ];
     });
-    return { grouppedShapes };
+    return grouppedShapes;
   }
 
   renderSteps(graph = false) {
     const { setPercentageInterval, percentageInterval } = this.props;
     const percentage = percentageCount();
-    const [minPercentage, maxPercentage] = percentageInterval;
+    const [minPercentage, maxPercentage] = percentageInterval as [number, number];
 
-    return percentage.map((currentPercent) => {
-      const activePercent = currentPercent >= minPercentage && currentPercent <= 100 ? 'active' : '';
-      const activeColumn = currentPercent > minPercentage && currentPercent <= 100 ? 'active' : '';
-      const activeGlissor = currentPercent === minPercentage || currentPercent === maxPercentage ? 'active' : '';
-
+    return percentage.map((currentPercent: number) => {
+      const activePercent: string = currentPercent >= minPercentage && currentPercent <= 100 ? 'active' : '';
+      const activeColumn: string = currentPercent > minPercentage && currentPercent <= 100 ? 'active' : '';
+      const activeGlissor: string =
+        currentPercent === minPercentage || currentPercent === maxPercentage ? 'active' : '';
       return graph ? (
         <li
           key={`step_${currentPercent}`}
@@ -88,7 +97,7 @@ class FilterShapes extends Component {
 
   render() {
     const { percentageInterval, toggleShapesFiltering, isShapeFilteringVisible } = this.props;
-    const collapseTheme = {
+    const collapseTheme: { collapse: string; content: string } = {
       collapse: 'filter-shapes-collapse',
       content: 'filter-shapes-content',
     };
@@ -123,12 +132,13 @@ class FilterShapes extends Component {
   }
 }
 
-export default connect(
-  (state) => ({
-    currentShapes: similarShapesResults(state),
-    percentageInterval: getPercentageInterval(state),
-  }),
-  (dispatch) => ({
-    setPercentageInterval: (value) => dispatch(updateCurrentPercentage(value)),
-  }),
-)(FilterShapes);
+const mapState = (state: RootState) => ({
+  currentShapes: similarShapesResults(state),
+  percentageInterval: getPercentageInterval(state),
+});
+
+const mapDispatch = (dispatch: Function) => ({
+  setPercentageInterval: (value) => dispatch(updateCurrentPercentage(value)),
+});
+
+export default connect<StateProps, DispatchProps, {}, RootState>(mapState, mapDispatch)(FilterShapes);
