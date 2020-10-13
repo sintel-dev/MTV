37650f49ded712e191a192660ba5ee387c557849
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2020-10-13 11:08:49 +0300

    Droppable typescript component

--- client/src/components/Timeseries/UploadEvents/Droppable.tsx
+++ client/src/components/Timeseries/UploadEvents/Droppable.tsx
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
+import React, { ReactNode } from 'react';
 import Dropzone from 'react-dropzone';
 import { FolderIcon } from '../../Common/icons';
 
-class Droppable extends Component {
-  listAddedFiles() {
-    const { currentFiles } = this.props;
-    return (
-      (currentFiles.length && (
-        <ul className="existing-files">
-          {currentFiles.map((file) => (
-            <li key={`${file.name}${file.path}`}>
-              <FolderIcon />
-              <span>{file.name}</span>
-              <i className="remove-file" onClick={() => this.props.onRemoveFile(file)}>
-                x
-              </i>
-            </li>
-          ))}
-        </ul>
-      )) ||
-      null
-    );
-  }
+type Props = {
+  onAddFiles: Function;
+  onRemoveFile: Function;
+  currentFiles: Array<any>;
+};
 
-  render() {
-    return (
-      <Dropzone accept="application/json" onDrop={(acceptedFiles) => this.props.onAddFiles(acceptedFiles)}>
-        {({ getRootProps, getInputProps }) => (
-          <section className="drop-zone">
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>
-                Drag and drop a file here, <span>or choose file</span>
-              </p>
-            </div>
-            {this.listAddedFiles()}
-          </section>
-        )}
-      </Dropzone>
-    );
-  }
-}
+const Droppable: React.FC<Props> = ({ onAddFiles, onRemoveFile, currentFiles }: Props) => {
+  const listAddedFiles = (): ReactNode =>
+    (currentFiles.length && (
+      <ul className="existing-files">
+        {currentFiles.map((file) => (
+          <li key={`${file.name}${file.path}`}>
+            <FolderIcon />
+            <span>{file.name}</span>
+            <i className="remove-file" onClick={() => onRemoveFile(file)}>
+              x
+            </i>
+          </li>
+        ))}
+      </ul>
+    )) ||
+    null;
+
+  return (
+    <Dropzone accept="application/json" onDrop={(acceptedFiles) => onAddFiles(acceptedFiles)}>
+      {({ getRootProps, getInputProps }) => (
+        <section className="drop-zone">
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>
+              Drag and drop a file here, <span>or choose file</span>
+            </p>
+          </div>
+          {listAddedFiles()}
+        </section>
+      )}
+    </Dropzone>
+  );
+};
 
 export default Droppable;
